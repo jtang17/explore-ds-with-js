550ac4bcf64badbf39e92b8ad8fc2e735f36d940
Lisa Gee <zhusufeng@gmail.com>
2017-10-31 17:16:46 -0700

    Add examples for primitive data types

--- native-data-types.js
+++ native-data-types.js
@@ -1,10 +1,31 @@
 /* PRIMITIVE */
 // String
-// Number
-// Boolean
-// Symbol
+let myString = 'Hello, World!';
+console.log('Here is myString: ', myString);
 
+// Number
+let myNumber = 9248;
+console.log('Here is myNumber: ', myNumber);
+
+// Boolean
+let isThisMyNumber = true;
+console.log('Here is isThisMyNumber: ', isThisMyNumber);
+
+// Symbol
+// How to create a new Symbol
+// This is called factory function
+const myFirstSymbol = Symbol();
+const mySecondSymbol = Symbol();
+
+// Each Symbol is unique
+console.log(myFirstSymbol !== mySecondSymbol);
+
+
+
+
+/* SPECIAL */
 // Null
 // Undefined
 
+/* COMPOSITE */
 // Object
