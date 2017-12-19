f3d99a93f02a76220c70350ba2e7aa457200a4cb
Lisa Gee <zhusufeng@gmail.com>
2017-12-18 16:12:13 -0800

    Rename myCoolArr, semiArr and add for loop

--- loops.js
+++ loops.js
@@ -1,12 +1,15 @@
-var myCoolArr = [0, 1, 2, 3, null, undefined, true];
+var arr = [0, 1, 2, 3, null, undefined, true];
 
-myCoolArr['four'] = 4;
+arr['four'] = 4;
 
-var semiArr = [100, 200]
-myCoolArr[semiArr] = [5, 6]
+var innerArr = [100, 200]
+arr[innerArr] = [5, 6]
 
-delete myCoolArr[2];
+delete arr[2];
 
-console.log(myCoolArr);
+console.log(arr);
 
-myCoolArr.forEach((el, i) => console.log(`forEach: i is ${i}, el is ${el}`));
+arr.forEach((el, i) => console.log(`forEach: i is ${i}, el is ${el}`));
+
+for (let i = 0; i < arr.length; i++) console.log(`for: i is ${i}, arr[i] is ${arr[i]}`);
+
