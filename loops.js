e008e6873e4bacd0947b3acf2aa34473960ae6ea
Lisa Gee <zhusufeng@gmail.com>
2017-12-18 16:36:49 -0800

    Add more info on for ... in loops

--- loops.js
+++ loops.js
@@ -4,19 +4,30 @@ arr['four'] = 4;
 
 var innerArr = [100, 200]
 arr[innerArr] = [5, 6]
+Array.prototype.arrCustom = function() {};
+
+var mySymbol = Symbol('7');
 
 delete arr[2];
 
-console.log(arr);
+console.log(arr); // [ 0, 1, empty, 3, null, undefined, true, four: 4, '100,200': [ 5, 6 ] ]
 
+/* FOR */
+for (let i = 0; i < arr.length; i++) console.log(`for: i is ${i}, arr[i] is ${arr[i]}`);
+
+/* FOR ... IN */
+// for (let key in arr) console.log(`for in: key is ${key}, value is ${arr[key]}`);
+  // for ... in iterates over the enumerable properties.
+  // Index 2 (or key 2) was deleted, it is now undefined,
+  //   so it was not iterated over which is not enumerable.
+  // Symbols are also non-enumerable, so it was not iterated over either.
+
+/* FOR ... OF */
+// for (let value of arr) console.log(`for of: value is ${value}`);
+  // for ... of iterates over data that the iterable object defines to be iterated over.
+  // So in an array, only those with numeric keys are iterated over.
+  // for ... of did not iterate over the keys 'four' or [100, 200].
+  // for ... of can terminate by use of break, continue, throw, return.
+
+/* FOREACH */
 // arr.forEach((el, i) => console.log(`forEach: i is ${i}, el is ${el}`));
-
-// for (let i = 0; i < arr.length; i++) console.log(`for: i is ${i}, arr[i] is ${arr[i]}`);
-
-// for (let i in arr) console.log(`for in: i is ${i}, arr[i] is ${arr[i]}`);
-
-for (let value of arr) console.log(`for of: value is ${value}`);
-// for of iterates over data that the iterable object defines to be iterated over
-// so in an array, only those with numeric keys are iterated over
-// it does not iterate over key 'four' or [100, 200]
-// for of can terminate by use of break, continue, throw, return
