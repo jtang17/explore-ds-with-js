982ef8625e4e3c7a8cd4fe8241bbbd1d58bd7f78
Lisa Gee <zhusufeng@gmail.com>
2017-12-18 16:22:27 -0800

    Add for ... in and for ... of loops

--- loops.js
+++ loops.js
@@ -9,7 +9,14 @@ delete arr[2];
 
 console.log(arr);
 
-arr.forEach((el, i) => console.log(`forEach: i is ${i}, el is ${el}`));
+// arr.forEach((el, i) => console.log(`forEach: i is ${i}, el is ${el}`));
 
-for (let i = 0; i < arr.length; i++) console.log(`for: i is ${i}, arr[i] is ${arr[i]}`);
+// for (let i = 0; i < arr.length; i++) console.log(`for: i is ${i}, arr[i] is ${arr[i]}`);
 
+// for (let i in arr) console.log(`for in: i is ${i}, arr[i] is ${arr[i]}`);
+
+for (let value of arr) console.log(`for of: value is ${value}`);
+// for of iterates over data that the iterable object defines to be iterated over
+// so in an array, only those with numeric keys are iterated over
+// it does not iterate over key 'four' or [100, 200]
+// for of can terminate by use of break, continue, throw, return
