7d6cae87ff4603ad1c30cc986bdf123cc88929ce
Lisa Gee <zhusufeng@gmail.com>
2017-12-20 12:52:55 -0800

    Rename file to other.js

---
+++ other.js
@@ -0,0 +1,36 @@
+// /* SPECIAL */
+// // Null
+// // Reminder null is the intentional absence of any object value
+// let answer1 = null;
+// console.log('Here is answer1: ', answer1);
+
+
+
+
+
+
+// // Undefined
+// // Reminder undefined is a variable that has not been assigned a value
+// let answer2;
+// console.log('Here is answer2: ', answer2);
+
+// // Note undefined is different than not defined
+// // not defined is not a data type but an error message
+// // answer3 has not been created
+// // console.log('Here is answer3: ', answer3);
+
+
+
+
+
+
+// /* COMPOSITE */
+// // Object
+// const hedwig = {
+//   type: 'owl',
+//   age: '10',
+//   color: 'white',
+//   owner: 'Harry Potter'
+// };
+
+// console.log('Here is hedwig: ', hedwig);
