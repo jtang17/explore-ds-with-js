fe2c7d5cbc3fffc75ccecdd099def0ca3587365f
Lisa Gee <zhusufeng@gmail.com>
2017-12-20 20:34:04 -0800

    Update for loop use cases

--- loops.js
+++ loops.js
@@ -34,7 +34,7 @@ console.log(arr.length); // 7
     Using numeric properties, it will not iterate over non-numeric keys
     such as 'four', [100, 200], or mySymbol.
 
-  Use cases: Arrays, (Objects), Great when you need to use the index or specific pattern
+  Use cases: Arrays, String, arguments object, (Objects), Great when you need to use the index or specific pattern
 
   Termination: break, continue, throw, return
 */
